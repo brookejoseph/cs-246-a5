acea4bfb5a7e5e7473243d3b4f39fd7384f9c983
Brooke Joseph <brookecoco123123@gmail.com>
2024-11-19 13:54:41 -0500

    pushing changes to bloch.h

--- block/block.h
+++ block/block.h
@@ -1,1 +1,36 @@
+// the main block interface file
+#include <vector>
+#include "cell/cell.h" // to add
 
+enum state
+{
+    ccw,
+    cw,
+    left,
+    right,
+    rotate
+};
+
+class Block
+{
+private:
+    vector<Cell> cells;
+    state state;
+
+public:
+    Block();
+    ~Block();
+    Block(Block const &other) {};
+    Block(Block &&other) {};
+    Block &operator=(const Block &other) {};
+    Block &operator+=(const Block &other) {}; // potentially useful for merging the board and block
+    Block &operator-=(const Block &other) {};
+
+    void heavy();
+    void virtual rotateccw();
+    void virtual rotatecw();
+    void virtual left();
+    void virtual right();
+    void virtual down();
+    void virtual drop();
+};
