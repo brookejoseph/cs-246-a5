dcd8156d6a7d08015cc21065feff9c080933dbee
Brooke Joseph <brookecoco123123@gmail.com>
2024-11-19 15:45:33 -0500

    finished the board.h

--- board/board.h
+++ board/board.h
@@ -4,10 +4,43 @@
 #include <ostream>
 #include "block/block.h"
 #include "level/level.h"
+#include "cell/cell.h"
 
 class Board
 {
 private:
     Block *current_block;
+    Block *next_block;
     Level *current_level;
-}
+    vector<vector<cell>> board;
+    int score;
+
+public:
+    Board();
+    ~Board();
+    Board(Board const &other) {};
+    Board(Board &&other) {};
+
+    bool isValidMove() {};
+    int findScore() {};
+
+    void levelDown() {};
+    void levelUp() {};
+    Level getLevelPtr() {};
+
+    void left(int amount) {};
+    void right(int amount) {};
+    void down(int amount) {};
+    void cw(int amount) {};
+    void ccw(int amount) {};
+    void drop(int amount) {};
+
+    void saveGame() {};
+    void restart() {};
+    void loadGame() {};
+
+    void eraseBlock() {};
+    void addCell(Board &block) {};
+    void addToTurnCount() {};
+    int getTurnCount() {};
+};
