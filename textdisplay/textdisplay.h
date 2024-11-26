115193142eecbaf17b44351014fdc749610288eb
yannajask <yannajask@gmail.com>
2024-11-26 17:12:40 -0500

    PLEASE FIX TESTDISPLAYYY

--- textdisplay/textdisplay.h
+++ textdisplay/textdisplay.h
@@ -13,11 +13,10 @@ private:
 
     int dimX;
     int dimY;
-    int space = 7; // how many chars to seperate boards
+    int sep = 7; // how many chars to seperate boards
 
-    int numDigits(int x) const; // counts the number of digits in an integer
-    void printHeader(const string &text) const;
-    void printValues(const string &text, int value1, int value2) const;
+    void printHeaders(const std::string &text) const;
+    void printValues(const std::string &text, int value1, int value2) const;
     void printDashes() const;
     void printBoards() const;
     void printNextBlocks() const;
@@ -26,6 +25,6 @@ public:
     TextDisplay(std::shared_ptr<Board> player1, std::shared_ptr<Board> player2, int dimX, int dimY);
     ~TextDisplay();
     virtual void notify() override;
-}
+};
 
 #endif
